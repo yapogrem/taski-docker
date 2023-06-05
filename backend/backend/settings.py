49712e411fbbfb3316196189caa4073da52200d1
pogrem <pogrem@gmail.com>
2023-06-05 11:37:25 +0300

    set

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,7 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
